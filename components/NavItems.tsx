cbc00467f1ff1c23facbb199997a46af2d052cb6
s1mpleog <mrzkk07@gmail.com>
2024-01-15 14:07:40 +0530

    created navbar

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -1,7 +1,57 @@
-const NavItems = () => {
-  return (
-    <div>NavItems</div>
-  )
-}
+"use client";
 
-export default NavItems
+import { useEffect, useRef, useState } from "react";
+import { PRODUCT_CATEGORIES } from "@/config";
+import NavItem from "./NavItem";
+import { useOnClickOutside } from "@/hooks/use-on-click-outside";
+
+const NavItems = () => {
+  const [activeIndex, setActiveIndex] = useState<null | number>(null);
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        setActiveIndex(null);
+      }
+    };
+    document.addEventListener("keydown", handler);
+
+    return () => {
+      document.removeEventListener("keydown", handler);
+    };
+  }, []);
+
+  const isAnyOpen = activeIndex !== null;
+
+  const navRef = useRef<HTMLDivElement | null>(null);
+
+  useOnClickOutside(navRef, () => setActiveIndex(null));
+
+  return (
+    <div className="flex gap-4 h-full" ref={navRef}>
+      {PRODUCT_CATEGORIES.map((category, i) => {
+        const handleOpen = () => {
+          if (activeIndex === i) {
+            setActiveIndex(null);
+          } else {
+            setActiveIndex(i);
+          }
+        };
+
+        const isOpen = i === activeIndex;
+
+        return (
+          <NavItem
+            category={category}
+            handleOpen={handleOpen}
+            isOpen={isOpen}
+            isAnyOpen={isAnyOpen}
+            key={category.value}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default NavItems;
